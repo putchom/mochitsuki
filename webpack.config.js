7298652825bd3e20612239045e7ce44c3062e6eb
putchom <putchom8@gmail.com>
2022-01-02 01:55:05 +0900

    root

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ module.exports = {
   mode: "development",
   entry: "./src/index.ts",
   output: {
-    path: `${__dirname}/dist`,
+    path: `${__dirname}`,
     filename: "main.js"
   },
   module: {
